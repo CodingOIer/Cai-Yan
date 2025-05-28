8b3ccb71731b4149ee3dcfdc51a9fa965829127d
Hongtian Wang <i@codingoier.com>
2025-05-28 16:47:25 +0800

    :sparkles: 完成猜罪配置

--- scripts/main.py
+++ scripts/main.py
@@ -2,13 +2,15 @@ import os
 import time
 import doctor
 import cooking
+import crime
 import song
 
 menu = '''
 Cai-Yan 工具合计，目前已有：
 1. AI 医生助手
 2. AI 厨师助手
-3. 暴力枚举猜歌助手（速度较慢，不建议使用）
+3. AI 猜罪助手
+4. 暴力枚举猜歌助手（速度较慢，不建议使用）
 对应的 AI 提示词可以在 /sciripts/prompt 中查看与修改，默认为利于游戏胜利的提示词。
 部分游戏会唤起 Chrome 浏览器自动操作，请不要在唤起的 Chrome 中操作。
 '''
@@ -27,6 +29,10 @@ if __name__ == '__main__':
             os.system('cls' if os.environ.get('OS') == 'Windows_NT' else 'clear')
             cooking.main()
         if id == '3':
+            time.sleep(0.5)
+            os.system('cls' if os.environ.get('OS') == 'Windows_NT' else 'clear')
+            crime.main()
+        if id == '4':
             time.sleep(0.5)
             os.system('cls' if os.environ.get('OS') == 'Windows_NT' else 'clear')
             song.main()
