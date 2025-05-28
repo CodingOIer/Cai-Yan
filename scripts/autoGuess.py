6414a241620d517ba970e45e56e02da0ce41381a
Hongtian Wang <i@codingoier.com>
2025-05-28 20:15:57 +0800

    :bug: 添加退出，修复未加载设置

--- scripts/autoGuess.py
+++ scripts/autoGuess.py
@@ -7,6 +7,7 @@ INPUT_DELAY = 0.1
 
 
 def loadSettings():
+    global INPUT_DELAY
     try:
         with open('./settings.json', 'r', encoding='utf-8') as f:
             data = json.load(f)
@@ -21,9 +22,18 @@ def loadChars(filepath):
 
 
 def main():
+    loadSettings()
     chars = loadChars('./charset/chinese-6750.txt')
+    print(f'已加载 {len(chars)} 个字符')
+    print('请注意，如果需要停止代码，请按住 Esc 键')
+    input(
+        '按下回车后 2s 将开始输入字符，请在此期间将点击输入框，不要做其他操作，等待程序填写'
+    )
     time.sleep(2)
     for current in chars:
+        if keyboard.is_pressed('esc'):
+            print("检测到 Esc，退出程序")
+            break
         pyperclip.copy(current)
         keyboard.send('ctrl+a')
         keyboard.send('delete')
